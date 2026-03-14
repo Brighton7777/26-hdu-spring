b5947c735a71a40d45d7176cb553e3e0cf89b462
Brighton7777 <1205704350@qq.com>
2026-03-14 12:29:29 +0800

    add note for warm-up problem 1

--- warm-up/src/1.cpp
+++ warm-up/src/1.cpp
@@ -10,26 +10,22 @@ int main()
     ios::sync_with_stdio(0);
     int T;
     cin >> T;
-    while(T--) {
+    while (T--)
+    {
         int n;
         cin >> n;
         int ans = 1;
-        if(n == 4) {
-            printf("1\n");
-            continue;
-        }
-        if(n == 6 || n == 8) {
-            printf("2\n");
-            continue;
-        }
-        for(int i = 3; i * i <= n; ++i) {
-            if(n % i == 0){
+        for (int i = 3; i * i <= n; ++i)
+        {
+            if (n % i == 0)
+            {
                 ans = n / i;
                 break;
-            } 
+            }
         }
-        if(ans == 1 && n % 2 == 0) {
-            ans = 2;
+        if (n % 2 == 0 && n / 2 >= 3)
+        {
+            ans = max(ans, 2);
         }
         printf("%d\n", ans);
     }
